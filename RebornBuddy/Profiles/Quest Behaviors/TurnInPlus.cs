20af558e0246fae02e49b0e91bcdb50dfe38cd77
y2krazy <y2krazy.profiles@gmail.com>
2015-06-07 14:55:11 -0500

    Changed directory structure.

---
+++ RebornBuddy/Profiles/Quest Behaviors/TurnInPlus.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Clio.Utilities;
+using Clio.XmlEngine;
+using ff14bot.Behavior;
+using ff14bot.Helpers;
+using ff14bot.Managers;
+using ff14bot.Navigation;
+using ff14bot.NeoProfiles;
+using ff14bot.RemoteWindows;
+using TreeSharp;
+using Action = TreeSharp.Action;
+
+namespace ff14bot.NeoProfiles
+{	
+	[XmlElement("TurnInPlus")]
+
+	class TurnInPlusTag : TurnInTag
+	{
+		public bool actiontaken = false;
+
+		protected override void OnDone()
+		{
+			actiontaken = false;
+			base.OnDone();
+		}
+
+		protected override Composite CreateBehavior()
+		{
+			return new PrioritySelector(
+				new Decorator(ret => QuestId == 65964 && SelectYesno.IsOpen,
+					new Action(r =>
+					{
+						SelectYesno.ClickYes();
+					})
+				),
+				base.CreateBehavior()
+				);
+		}
+	}
+}
