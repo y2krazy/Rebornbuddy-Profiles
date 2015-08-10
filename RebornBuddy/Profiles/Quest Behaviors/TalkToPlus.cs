add341ac4d1b39e2c94f0ac5c59d0fce96233c97
y2krazy <y2krazy.profiles@gmail.com>
2015-08-10 07:26:22 -0500

    Modified included quests.

--- RebornBuddy/Profiles/Quest Behaviors/TalkToPlus.cs
+++ RebornBuddy/Profiles/Quest Behaviors/TalkToPlus.cs
@@ -39,18 +39,6 @@ namespace ff14bot.NeoProfiles.Tags
                         SelectYesno.ClickYes();
                     })
                 ),
-                new Decorator(ret => QuestId == 67127 && SelectString.IsOpen,
-                    new Action(r =>
-                    {
-                        SelectString.ClickSlot(0);
-                    })
-                ),
-                new Decorator(ret => QuestId == 67130 && SelectString.IsOpen,
-                    new Action(r =>
-                    {
-                        SelectString.ClickSlot(0);
-                    })
-                ),
                 new Decorator(ret => QuestId == 67130 && SelectIconString.IsOpen,
                     new Action(r =>
                     {
