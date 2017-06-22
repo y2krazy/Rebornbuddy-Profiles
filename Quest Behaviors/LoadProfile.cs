d195c8b0603cb6fe6f91df60530b80ecf73e22fb
y2krazy <y2krazy.profiles@gmail.com>
2017-06-22 01:13:28 -0500

    Updated LoadProfile.cs

--- Quest Behaviors/LoadProfile.cs
+++ Quest Behaviors/LoadProfile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -27,7 +28,7 @@ namespace ff14bot.NeoProfiles
         private bool _done;
 
         [XmlAttribute("Path")]
-        public string Path { get; set; }
+        public string ProfileName { get; set; }
 
         public override bool IsDone { get { return _done; } }
 
@@ -37,7 +38,7 @@ namespace ff14bot.NeoProfiles
                 new Decorator(ret => TreeRoot.IsRunning,
                     new Action(r =>
                     {
-                        NeoProfileManager.Load(Path, true);
+                        NeoProfileManager.Load(NewProfilePath, true);
                         NeoProfileManager.UpdateCurrentProfileBehavior();
                         _done = true;
                     })
@@ -45,6 +46,35 @@ namespace ff14bot.NeoProfiles
             );
         }
 
+		private string NewProfilePath;
+		protected override void OnStart()
+        {
+			
+			var CurrentProfile = NeoProfileManager.CurrentProfile.Path;
+            // Support for store profiles.
+            // Absolute path to a store profile.
+            if (IsStoreProfile(ProfileName))
+            {
+                NewProfilePath = Slashify(ProfileName);
+                return;
+            }
+			
+            // Relative path to a store profile
+            if (IsStoreProfile(CurrentProfile))
+            {
+                NewProfilePath = Slashify(CurrentProfile + "/../" + ProfileName);
+                return;
+            }
+
+            // Convert path name to absolute, and canonicalize it...
+            var absolutePath = Path.Combine(Path.GetDirectoryName(CurrentProfile), ProfileName);
+            absolutePath = Path.GetFullPath(absolutePath);
+            var canonicalPath = new Uri(absolutePath).LocalPath;
+            NewProfilePath = Slashify(canonicalPath);
+			
+			Log("Changing profile to {0}",ProfileName);
+        }
+		
         /// <summary>
         /// This gets called when a while loop starts over so reset anything that is used inside the IsDone check
         /// </summary>
@@ -56,6 +86,18 @@ namespace ff14bot.NeoProfiles
         protected override void OnDone()
         {
 
+        }
+		
+		
+		private bool IsStoreProfile(string path)
+        {
+            return path.StartsWith("store://");
+        }
+
+        // Converts all slashes to back-slashes if path is local; otherwise converts all back-slashes to slashes
+        private string Slashify(string path)
+        {
+            return IsStoreProfile(path) ? path.Replace(@"\", "/") : path.Replace("/", @"\");
         }
     }
 }
