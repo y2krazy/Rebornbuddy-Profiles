937746cabd80e637cdf3043f4621e164887f9923
y2krazy <y2krazy.profiles@gmail.com>
2017-08-13 22:13:48 -0500

    Added 1 Quest to TurnInPlus

--- Quest Behaviors/TurnInPlus.cs
+++ Quest Behaviors/TurnInPlus.cs
@@ -264,6 +264,12 @@ namespace ff14bot.NeoProfiles
 						SelectYesno.ClickYes();
 					})
 				),
+                new Decorator(ret => QuestId == 68112 && SelectYesno.IsOpen,
+                    new Action(r =>
+                    {
+                        SelectYesno.ClickYes();
+                    })
+                ),
 				new Decorator(ret => QuestId == 68291 && SelectYesno.IsOpen,
 					new Action(r =>
 					{
