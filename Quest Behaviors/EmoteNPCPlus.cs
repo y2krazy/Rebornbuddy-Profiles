c5fb10c42b1cc4c19a6f13895e087243c9f5e0c4
y2krazy <y2krazy.profiles@gmail.com>
2017-09-03 11:38:38 -0500

    Added 1 Quest to EmoteNPCPlus

--- Quest Behaviors/EmoteNPCPlus.cs
+++ Quest Behaviors/EmoteNPCPlus.cs
@@ -35,6 +35,12 @@ namespace ff14bot.NeoProfiles.Tags
                             SelectYesno.ClickYes();
                         })
                     ),
+                    new Decorator(ret => QuestId == 67954 && SelectString.IsOpen,
+                        new Action(r =>
+                        {
+                            SelectString.ClickSlot(0);
+                        })
+                    ),
                     base.CustomLogic
                     );
             }
